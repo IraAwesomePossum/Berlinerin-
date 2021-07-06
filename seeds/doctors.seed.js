193f6d5840d71c7c23a2a908426b2193d7df1dba
Irina <ifalko@protonmail.com>
2021-07-06 18:56:54 +0200

    styles added

--- seeds/doctors.seed.js
+++ seeds/doctors.seed.js
@@ -13,7 +13,7 @@ const doctors = [
     specialisation: "Family Doctor",
     averageRating: 4.7,
     address: "Nassauische Straße 25",
-    img: "https://source.unsplash.com/random/300x300",
+    img: "https://source.unsplash.com/FVh_yqLR9eA/300x300",
     followers: 150,
   },
   {
@@ -22,7 +22,7 @@ const doctors = [
     specialisation: "ObGyn",
     averageRating: 4.9,
     address: "Manfred-von-Richthofen-Str. 9",
-    img: "https://source.unsplash.com/random/300x300",
+    img: "https://source.unsplash.com/pTrhfmj2jDA/300x300",
     followers: 250,
   },
   {
@@ -31,7 +31,7 @@ const doctors = [
     specialisation: "Specialist in internal medicine",
     averageRating: 4.5,
     address: "Dieffenbachstraße 65",
-    img: "https://source.unsplash.com/random/300x300",
+    img: "https://source.unsplash.com/MQ2xYBHImKM/300x300",
     followers: 190,
   },
   {
@@ -40,7 +40,7 @@ const doctors = [
     specialisation: "Family Doctor",
     averageRating: 4.5,
     address: "Windscheidstraße 31",
-    img: "https://source.unsplash.com/random/300x300",
+    img: "https://source.unsplash.com/GnLuuG9crEY/300x300",
     followers: 100,
   },
   {
@@ -49,7 +49,7 @@ const doctors = [
     specialisation: "Dermatologist",
     averageRating: 4.6,
     address: "Leipziger Str. 112",
-    img: "https://source.unsplash.com/random/300x300",
+    img: "https://source.unsplash.com/rm7rZYdl3rY/300x300",
     followers: 177,
   },
   {
@@ -58,7 +58,7 @@ const doctors = [
     specialisation: "Family Doctor",
     averageRating: 4.9,
     address: "Schwedter Str. 9",
-    img: "https://source.unsplash.com/random/300x300",
+    img: "https://source.unsplash.com/UnKSOLGlMEc/300x300",
     followers: 230,
   },
   {
@@ -67,7 +67,7 @@ const doctors = [
     specialisation: "Cardiologist",
     averageRating: 4.7,
     address: "Charlottenstraße 63",
-    img: "https://source.unsplash.com/random/300x300",
+    img: "https://source.unsplash.com/yHTDa4FIv3o/300x300",
     followers: 109,
   },
   {
@@ -76,7 +76,7 @@ const doctors = [
     specialisation: "Family Doctor",
     averageRating: 4.7,
     address: "Feuerbaherstr. 89",
-    img: "https://source.unsplash.com/random/300x300",
+    img: "https://source.unsplash.com/7bMdiIqz_J4/300x300",
     followers: 167,
   },
 ];
